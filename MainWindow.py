532fbc8634c83c86c46ed0160096363e80e73d11
Shreyas Potnis <shreyaspotnis@gmail.com>
2014-01-20 11:28:16 -0500

    Started adding UI elements.

--- MainWindow.py
+++ MainWindow.py
@@ -1,7 +1,13 @@
+"""
+run  python2-pyuic4 -xo ui_MainWindow.py MainWindow.ui on the command line
+before running the application.
+"""
+
 from PyQt4 import QtGui
+from ui_MainWindow import Ui_MainWindow
 
 
-class MainWindow(QtGui.QMainWindow):
+class MainWindow(QtGui.QMainWindow, Ui_MainWindow):
     """The only window of the application."""
 
     def __init__(self, config, internalConfig):
@@ -9,9 +15,5 @@ class MainWindow(QtGui.QMainWindow):
 
         self.config = config
         self.internalConfig = internalConfig
-        self.initUI()
 
-    def initUI(self):
-        """Add UI elements to the widget."""
-
-        self.statusBar()
+        self.setupUi(self)
---
+++ ui_MainWindow.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'MainWindow.ui'
+#
+# Created: Mon Jan 20 11:24:48 2014
+#      by: PyQt4 UI code generator 4.10.3
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt4 import QtCore, QtGui
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    def _fromUtf8(s):
+        return s
+
+try:
+    _encoding = QtGui.QApplication.UnicodeUTF8
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+except AttributeError:
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig)
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName(_fromUtf8("MainWindow"))
+        MainWindow.resize(800, 600)
+        self.centralwidget = QtGui.QWidget(MainWindow)
+        self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
+        self.gridLayoutWidget = QtGui.QWidget(self.centralwidget)
+        self.gridLayoutWidget.setGeometry(QtCore.QRect(9, 9, 301, 191))
+        self.gridLayoutWidget.setObjectName(_fromUtf8("gridLayoutWidget"))
+        self.gridLayout = QtGui.QGridLayout(self.gridLayoutWidget)
+        self.gridLayout.setMargin(0)
+        self.gridLayout.setObjectName(_fromUtf8("gridLayout"))
+        self.comboLJInput = QtGui.QComboBox(self.gridLayoutWidget)
+        self.comboLJInput.setObjectName(_fromUtf8("comboLJInput"))
+        self.gridLayout.addWidget(self.comboLJInput, 0, 0, 1, 1)
+        self.pushAcquire = QtGui.QPushButton(self.gridLayoutWidget)
+        self.pushAcquire.setObjectName(_fromUtf8("pushAcquire"))
+        self.gridLayout.addWidget(self.pushAcquire, 0, 1, 1, 1)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtGui.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 23))
+        self.menubar.setObjectName(_fromUtf8("menubar"))
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtGui.QStatusBar(MainWindow)
+        self.statusbar.setObjectName(_fromUtf8("statusbar"))
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(_translate("MainWindow", "Manual DAQ", None))
+        self.pushAcquire.setText(_translate("MainWindow", "Acquire", None))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtGui.QApplication(sys.argv)
+    MainWindow = QtGui.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
+
